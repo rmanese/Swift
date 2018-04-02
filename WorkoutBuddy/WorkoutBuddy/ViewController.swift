616faa8b623619a1f4ca76ab2e91974be4cf838a
Roberto Manese III <rjmanese@gmail.com>
2018-04-02 12:44:29 -0700

    start

---
+++ WorkoutBuddy/WorkoutBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkoutBuddy
+//
+//  Created by Roberto Manese III on 4/2/18.
+//  Copyright © 2018 jawnyawn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
